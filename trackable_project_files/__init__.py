1024ba8c01011ff44ed7ada19fdc2127e370f231
Marco Bernasocchi <marco@opengis.ch>
2019-04-23 21:39:58 +0200

    warn when missing lxml
    fixes #1
    
    Signed-off-by: Marco Bernasocchi <marco@opengis.ch>

--- trackable_project_files/__init__.py
+++ trackable_project_files/__init__.py
@@ -1,17 +1,26 @@
 # encoding: utf-8
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 # Copyright (C) 2019 Matthias Kuhn
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 # Licensed under the terms of GNU GPL 2
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 
-import lxml.etree as ET
+
+try:
+    import lxml.etree as ET
+except ImportError as e:
+    message = "Missing lxml dependency. You can install by executing the following code in the QGIS Python console: "
+    message += "import subprocess; "
+    message += "subprocess.check_call(['python', '-m', 'pip', 'install', 'lxml'])"
+    raise ImportError(message)
+
 from qgis.core import QgsProject
+
 
 def classFactory(iface):
     return StableProjectFile(iface)
@@ -23,12 +32,14 @@ class StableProjectFile:
 
     def normalize_xml(self):
         fn = QgsProject.instance().fileName()
-        with open(fn, 'r') as file:
+        with open(fn, "r") as file:
             et = ET.parse(file)
         et.write_c14n(fn)
 
     def initGui(self):
-        self.project_saved_connection = QgsProject.instance().projectSaved.connect(self.normalize_xml)
+        self.project_saved_connection = QgsProject.instance().projectSaved.connect(
+            self.normalize_xml
+        )
 
     def unload(self):
         QgsProject.instance().projectSaved.disconnect(self.project_saved_connection)
