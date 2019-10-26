45721efd22404370153943a679edf4ad95758cf4
Mario Baranzini <mario@opengis.ch>
2019-10-26 10:34:50 +0200

    PEP8

--- trackable_project_files/__init__.py
+++ trackable_project_files/__init__.py
@@ -1,17 +1,18 @@
 # encoding: utf-8
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 # Copyright (C) 2019 Matthias Kuhn
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 # Licensed under the terms of GNU GPL 2
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 
 import lxml.etree as ET
 from qgis.core import QgsProject
+
 
 def classFactory(iface):
     return StableProjectFile(iface)
@@ -28,7 +29,8 @@ class StableProjectFile:
         et.write_c14n(fn)
 
     def initGui(self):
-        self.project_saved_connection = QgsProject.instance().projectSaved.connect(self.normalize_xml)
+        self.project_saved_connection = \
+            QgsProject.instance().projectSaved.connect(self.normalize_xml)
 
     def unload(self):
         p = QgsProject.instance()
